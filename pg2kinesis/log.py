aa3c0f20a72e965b1cb9c450a28e696c06e8e13f
John Louis Del Rosario <john2x@gmail.com>
2019-10-11 13:36:21 +1300

    Parentheses are wrong

--- pg2kinesis/log.py
+++ pg2kinesis/log.py
@@ -4,4 +4,4 @@ import logging
 FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger()
-logger.setLevel(os.getenv('PG2KINESIS_LOG_LEVEL'), logging.INFO)
+logger.setLevel(os.getenv('PG2KINESIS_LOG_LEVEL', logging.INFO))
